45a0a89033ab7bedea612b0908a62dcfa07b989d
Fabien Potencier <fabien.potencier@gmail.com>
2010-06-23 23:02:26 +0200

    updated the vendors

---
+++ hello/HelloCache.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__.'/HelloKernel.php';
+
+use Symfony\Foundation\Cache\Cache;
+
+class HelloCache extends Cache
+{
+}
--- src/vendor/symfony/src/Symfony/Components/HttpKernel/ParameterBag.php
+++ src/vendor/symfony/src/Symfony/Components/HttpKernel/ParameterBag.php
@@ -53,6 +53,16 @@ class ParameterBag
     }
 
     /**
+     * Adds parameters.
+     *
+     * @param array $parameters An array of parameters
+     */
+    public function add(array $parameters = array())
+    {
+        $this->parameters = array_replace($this->parameters, $parameters);
+    }
+
+    /**
      * Returns a parameter by name.
      *
      * @param string $key     The key
@@ -69,7 +79,7 @@ class ParameterBag
      * @param string $key   The key
      * @param mixed  $value The value
      */
-    public function set($key, $value, $replace = true)
+    public function set($key, $value)
     {
         $this->parameters[$key] = $value;
     }
