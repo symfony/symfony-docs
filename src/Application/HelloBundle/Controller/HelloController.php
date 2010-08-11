c17a73a555ab12cde982d3c9f3543e914cca332d
Fabien Potencier <fabien.potencier@gmail.com>
2010-08-11 08:39:56 +0200

    added Twig templates

--- src/Application/HelloBundle/Controller/HelloController.php
+++ src/Application/HelloBundle/Controller/HelloController.php
@@ -9,5 +9,8 @@ class HelloController extends Controller
     public function indexAction($name)
     {
         return $this->render('HelloBundle:Hello:index', array('name' => $name));
+
+        // render a Twig template instead
+        // return $this->render('HelloBundle:Hello:index:twig', array('name' => $name));
     }
 }
