e78c7c4636a05f3c3301b839eed4159bb990edf3
Christian Flothmann <christian.flothmann@xabbuh.de>
2014-04-07 17:30:43 +0200

    modify Symfony CMF configuration values in the build process so that they match the Symfony documentation

--- make.bat
+++ make.bat
@@ -77,17 +77,17 @@ qthelp:
 	@echo
 	@echo "Build finished; now you can run "qcollectiongenerator" with the" \
 	      ".qhcp project file in $(BUILDDIR)/qthelp, like this:"
-	@echo "# qcollectiongenerator $(BUILDDIR)/qthelp/SymfonyCMF.qhcp"
+	@echo "# qcollectiongenerator $(BUILDDIR)/qthelp/Symfony.qhcp"
 	@echo "To view the help file:"
-	@echo "# assistant -collectionFile $(BUILDDIR)/qthelp/SymfonyCMF.qhc"
+	@echo "# assistant -collectionFile $(BUILDDIR)/qthelp/Symfony.qhc"
 
 devhelp:
 	$(SPHINXBUILD) -b devhelp $(ALLSPHINXOPTS) $(BUILDDIR)/devhelp
 	@echo
 	@echo "Build finished."
 	@echo "To view the help file:"
-	@echo "# mkdir -p $$HOME/.local/share/devhelp/SymfonyCMF"
-	@echo "# ln -s $(BUILDDIR)/devhelp $$HOME/.local/share/devhelp/SymfonyCMF"
+	@echo "# mkdir -p $$HOME/.local/share/devhelp/Symfony"
+	@echo "# ln -s $(BUILDDIR)/devhelp $$HOME/.local/share/devhelp/Symfony"
 	@echo "# devhelp"
 
 epub:
