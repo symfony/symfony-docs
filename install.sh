de7d2064e9d72c7b674c5589b854977bb9c77d55
Christian Flothmann <christian.flothmann@xabbuh.de>
2014-04-11 14:16:24 +0200

    ensure that destination directories don't exist before creating them

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ function sparse_checkout {
     git remote add -f origin http://github.com/$1/$2
     echo Resources/doc > .git/info/sparse-checkout
     git checkout master
+    rm -rf ../bundles/$2
     mv Resources/doc ../bundles/$2
     cd ..
     rm -rf sparse_checkout
@@ -18,5 +19,6 @@ sparse_checkout sensiolabs SensioGeneratorBundle
 sparse_checkout doctrine DoctrineFixturesBundle
 sparse_checkout doctrine DoctrineMigrationsBundle
 sparse_checkout doctrine DoctrineMongoDBBundle
+rm -rf cmf
 git clone http://github.com/symfony-cmf/symfony-cmf-docs cmf
 
