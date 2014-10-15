af6b70b48343151130370d0e569aca993d8187e5
Javier Eguiluz <javier.eguiluz@gmail.com>
2014-10-15 16:24:11 +0200

    added the missing "best-practice" directive import in Sphinx configuration

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ from pygments.lexers.agile import RubyLexer
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
-    'sensio.sphinx.refinclude', 'sensio.sphinx.configurationblock', 'sensio.sphinx.phpcode']
+    'sensio.sphinx.refinclude', 'sensio.sphinx.configurationblock', 'sensio.sphinx.phpcode', 'sensio.sphinx.bestpractice']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
