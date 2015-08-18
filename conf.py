5f952b038962fa304b1ae16b1858b89d320162b2
Wouter J <waldio.webdesign@gmail.com>
2015-08-18 13:19:43 +0200

    Update config

--- conf.py
+++ conf.py
@@ -130,7 +130,7 @@ api_url = 'http://api.symfony.com/master/%s'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
